64d07c9ba1e9b0e28d41966ca3400004afb96392
dileepkumarj <dileepkumar.j@techsimplified.net>
2017-05-21 20:39:01 +0530

    add changes

--- web/app/config/router.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Phalcon\Mvc\Router;
-
-$router = $di->getRouter();
-
-// Define your routes here
-
-$router->handle();
-
--- web/app/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-$router->add('/login-testing', array( 
-   'controller' => 'Jobs', 
-   'action' => 'Index', 
-));
-
-?>
